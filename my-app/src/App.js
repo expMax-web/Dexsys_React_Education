e6b6882f623ffb136941a495c7fdb6656333ba56
MaksimTimofeev <Maksim.Timofeev@homecredit.ru>
2021-10-11 11:13:56 +0400

    fix bugs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,22 @@
 import "./styles.css";
+import Navbar from "./components/Navbar/Navbar";
+import CharacterCard from "./components/CharacterCard/CharacterCard";
+import UpButton from "./components/UpButton/UpButton";
 
 function App() {
   const CharacterCards = [];
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <main className="Container">
+        <CharacterCard />
+        <CharacterCard />
+        <CharacterCard />
+        <CharacterCard />
+      </main>
+      <UpButton>Вверх</UpButton>
+    </div>
+  );
 }
 
 export default App;
