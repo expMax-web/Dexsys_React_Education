50b5025e77f24961119f4250c3d886bc05c5f400
MaksimTimofeev <Maksim.Timofeev@homecredit.ru>
2021-10-12 10:06:25 +0400

    fixed UpButton

--- my-app/src/components/CardsContainer/CardsContainer.jsx
+++ my-app/src/components/CardsContainer/CardsContainer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 import { CharacterCard } from "../CharacterCard/CharacterCard";
 import { UpButton } from "../UpButton/UpButton";
@@ -6,6 +6,33 @@ import { UpButton } from "../UpButton/UpButton";
 import styles from "./CardsContainer.module.scss";
 
 export const CardsContainer = ({ data }) => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 300) {
+      setShowScroll(true);
+      console.log(1);
+    }
+    if (showScroll && window.pageYOffset <= 300) {
+      setShowScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkScrollTop);
+    return () => {
+      window.removeEventListener("scroll", checkScrollTop);
+    };
+  });
+
+  const scrollToTop = () => {
+    console.log("GoUp");
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    setShowScroll(false);
+  };
   return (
     <main className={styles.Container}>
       {data?.characters?.results.map((character) => (
@@ -15,7 +42,7 @@ export const CardsContainer = ({ data }) => {
           key={character.id}
         />
       ))}
-      <UpButton>Вверх</UpButton>
+      {showScroll && <UpButton scrollToTop={scrollToTop}>Вверх</UpButton>}
     </main>
   );
 };
--- my-app/src/components/UpButton/UpButton.jsx
+++ my-app/src/components/UpButton/UpButton.jsx
@@ -1,38 +1,9 @@
-import Reacts, { useState, useEffect } from "react";
-
 import styles from "./UpButton.module.css";
 
-export const UpButton = ({ children }) => {
-  const [showScroll, setShowScroll] = useState(false);
-
-  const checkScrollTop = () => {
-    if (!showScroll && window.pageYOffset > 300) {
-      setShowScroll(true);
-    }
-    if (showScroll && window.pageYOffset <= 300) {
-      setShowScroll(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", checkScrollTop);
-    return () => {
-      window.removeEventListener("scroll", checkScrollTop);
-    };
-  }, []);
-  const scrollToTop = () => {
-    console.log("GoUp");
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-    setShowScroll(false);
-  };
+export const UpButton = ({ children, scrollToTop }) => {
   return (
-    showScroll && (
-      <button onClick={scrollToTop} className={styles.ButtonUp}>
-        {children}
-      </button>
-    )
+    <button onClick={scrollToTop} className={styles.ButtonUp}>
+      {children}
+    </button>
   );
 };
