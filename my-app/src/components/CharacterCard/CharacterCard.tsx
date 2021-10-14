e85f7e9283bb07271f8f34c5f5ef22a5caf11ee2
MaksimTimofeev <Maksim.Timofeev@homecredit.ru>
2021-10-14 16:54:25 +0400

    added Link in CharacterCard

--- my-app/src/components/CharacterCard/CharacterCard.tsx
+++ my-app/src/components/CharacterCard/CharacterCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { Maybe } from "../../api/types";
 import { ImgNotFound } from "../ImgNotFound/ImgNotFound";
@@ -8,22 +9,29 @@ import styles from "./CharacterCard.module.css";
 interface CardsContainerProps {
   image: Maybe<string> | undefined;
   name: Maybe<string> | undefined;
+  id: any;
 }
 
 export const CharacterCard: React.FC<CardsContainerProps> = ({
   image,
   name,
+  id,
 }) => {
   return (
-    <figure className={styles.CharacterCard}>
-      {image && name ? (
-        <p className={styles.ImgContainer}>
-          <img src={image} alt={name} className={styles.CharacterImg} />
-        </p>
-      ) : (
-        <ImgNotFound />
-      )}
-      <figcaption className={styles.CharactersName}>{name}</figcaption>
-    </figure>
+    <Link
+      style={{ textDecoration: "none", color: "inherit" }}
+      to={`/character/${id}`}
+    >
+      <figure className={styles.CharacterCard} id={id}>
+        {image && name ? (
+          <p className={styles.ImgContainer}>
+            <img src={image} alt={name} className={styles.CharacterImg} />
+          </p>
+        ) : (
+          <ImgNotFound />
+        )}
+        <figcaption className={styles.CharactersName}>{name}</figcaption>
+      </figure>
+    </Link>
   );
 };
--- my-app/src/pages/CharacterPage.tsx
+++ my-app/src/pages/CharacterPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const CharacterPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Hello World</h1>
+      <h1>Character {id}</h1>
     </div>
   );
 };
