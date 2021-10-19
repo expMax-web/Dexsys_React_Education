3974226246dff0c77dfa82767b09c82a8d1f8ee6
MaksimTimofeev <Maksim.Timofeev@homecredit.ru>
2021-10-19 17:42:46 +0400

    added format date

--- my-app/src/pages/CharacterPage.tsx
+++ my-app/src/pages/CharacterPage.tsx
@@ -10,7 +10,14 @@ const CharacterPage: React.FC = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error {error.message}</p>;
   if (!id) return <h1>id not received, unable to download data</h1>;
-
+  const getFormatedDate = (invalidDate: string | null | undefined) => {
+    if (invalidDate) {
+      const validDate = new Date(invalidDate);
+      return `${validDate.getDate()}.${validDate.getMonth()}.${validDate.getFullYear()}`;
+    }
+    return;
+  };
+  getFormatedDate(character?.location?.created);
   return (
     <div>
       <BackButton>Back</BackButton>
@@ -18,13 +25,13 @@ const CharacterPage: React.FC = () => {
         name={character?.name}
         image={character?.image}
         gender={character?.gender}
-        created={character?.created}
+        created={getFormatedDate(character?.created)}
         status={character?.status}
         species={character?.species}
         locationName={character?.location?.name}
         locationType={character?.location?.type}
         locationDimension={character?.location?.dimension}
-        locationCreated={character?.location?.created}
+        locationCreated={getFormatedDate(character?.location?.created)}
       />
     </div>
   );
